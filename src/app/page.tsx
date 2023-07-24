91c782b2df25b202995f0f3694fed778361ce2cb
Richard Wallier <ealliero@gmail.com>
2023-07-24 20:52:51 -0300

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import Image from 'next/image'
-import styles from './page.module.css'
+import React from 'react';
+import Image from 'next/image';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
@@ -92,5 +92,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
